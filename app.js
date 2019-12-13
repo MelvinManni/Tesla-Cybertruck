a1f19a80137197d385cfaa574e1cbeb6ac373814
MelvinManni <melvinnnamani@gmail.com>
2019-12-13 05:22:49 +0100

    new commits

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var modDropdown = document.getElementById('model-dropdown'),
-    modMenu = document.getElementById('models');
-
-    modDropdown.addEventListener('click', toggleModels);
-
-    function toggleModels(e){
-
-        if (modMenu.style.display === 'none'){
-            modMenu.style.display = 'block';
-        }
-        else{
-            modMenu.style.display = 'none';
-        }
+    modMenu = document.getElementById('models'),
 
 
-        e.preventDefault
-    }
+        modDropdown.addEventListener('click', toggleModels);
+
+        function toggleModels(e){
+
+            if (modMenu.style.display === 'none'){
+                modMenu.style.display = 'block';
+                modMenu.style.transition = '5s';
+            }
+            else{
+                modMenu.style.display = 'none';
+            }
+
+
+            e.preventDefault
+        };
+
